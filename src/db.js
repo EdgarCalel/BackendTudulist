54a728e3c381d9c4aff4df9ebe6eeb52639f5f89
EdgarCalel <edgar_kll@hotmail.com>
2022-05-13 17:22:26 -0600

    backend completo

--- src/db.js
+++ src/db.js
@@ -3,13 +3,20 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,DATABASE_URL
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/todoList`, {
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/todoList`, {
+  const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   port:5432,
+  dialectOptions:{
+    ssl: {
+  require:true,
+    rejectUnauthorized:false
+   }                             
+    },
 });
 const basename = path.basename(__filename);
 
